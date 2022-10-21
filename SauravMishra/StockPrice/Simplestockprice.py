356afef5d65a7bd85e583fb32834dadcb9a78bbf
Saurav Mishra <sauravmishra623@gmail.com>
2022-10-21 09:49:40 +0530

    Added Stockprice predicting app

---
+++ SauravMishra/StockPrice/Simplestockprice.py
@@ -0,0 +1,32 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock **closing price** and ***volume*** of Google!
+""") # it is the header of the web application
+# We can customize it like, adding extra '##' will make the heading smaller.
+# use markdown cheatsheet
+
+#https://towardsdatascience.com/how-to-get-stack-data-using-python-c0de1df17e75
+
+# define the ticker symbol
+tickerSymbol='GOOGL'
+# get data on this ticker
+tickerData=yf.Ticker(tickerSymbol)
+
+# get the historical prices for this ticker
+tickerDf=tickerData.history(period='1d',start='2010-5-31',end='2022-9-13')
+
+# Open High Low Close Volume Dividends Stock Splits
+
+st.write("""
+### Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
