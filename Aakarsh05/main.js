e82d31532ae7294dc9331e653e01590165a5c614
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-10-29 09:34:17 +0530

    Updated with <sup> tag

--- Aakarsh05/main.js
+++ Aakarsh05/main.js
@@ -8,32 +8,32 @@ btn.addEventListener("click",function()
     let input4=document.getElementById("userinput4").value;
     if(input1 > Math.pow(input2,input3))
     {
-        result.innerHTML="Θ(n^"+Math.log(input1)+")";
+        result.innerHTML="Θ(n<sup>"+Math.log(input1)+"</sup>)";
     }
     else if((input1 === Math.pow(input2,input3)))
     {
         if(input4 > -1)
         {
-        result.innerHTML="Θ(n^"+Math.log(input1)+"log^"+(input4+1)+"n)";
+        result.innerHTML="Θ(n<sup>"+Math.log(input1)+"</sup>log<sup>"+(input4+1)+"</sup>n)";
         }
         else if(input4 === -1)
         {
-            result.innerHTML="Θ(n^"+Math.log(input1)+"loglogn)";
+            result.innerHTML="Θ(n<sup>"+Math.log(input1)+"</sup>loglogn)";
         }
         else
         {
-            result.innerHTML="Θ(n^"+Math.log(input1)+")";
+            result.innerHTML="Θ(n<sup>"+Math.log(input1)+"<s/up>)";
         }
     }
     else 
     {
         if(input4 >= 0)
         {
-            result.innerHTML="Θ(n^"+(input3)+"log^"+(input4)+"n)";
+            result.innerHTML="Θ(n<sup>"+(input3)+"</sup>log<sup>"+(input4)+"</sup>n)";
         }
         else
         {
-            result.innerHTML="Θ(n^"+(input3)+")";
+            result.innerHTML="Θ(n<sup>"+(input3)+"</sup>)";
         }
      }
 }) ;
